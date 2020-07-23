b6f7d1be87f8bb0df78e21184f8a5b868d3a5779
Chris Speight <paradisio15@gmail.com>
2020-07-22 20:06:07 -0500

    New Book is now a card item

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+let showNewBook = false;
 const container = document.querySelector("#container");
+let newBookBtn = document.querySelector(".newBookCard");
 let myLibrary = [];
 
 function Book(author, title, pages, read) {
@@ -14,6 +16,7 @@ function addBookToLibrary(author, title, pages, read) {
 }
 
 function render() {
+  container.innerHTML = "";
   myLibrary.forEach(function (e) {
     const div = document.createElement("div");
     div.classList.add("bookCard");
@@ -35,6 +38,46 @@ function render() {
     div.appendChild(read);
     container.appendChild(div);
   });
+
+  const div = document.createElement("div");
+  div.classList.add("bookCard", "newBookCard");
+  const title = document.createElement("h3");
+  title.textContent = "New Book";
+  div.appendChild(title);
+  container.appendChild(div);
+
+  newBookBtn = document.querySelector(".newBookCard");
+  newBookBtn.onclick = () => renderForm();
+}
+
+function renderForm() {
+  const formContent = `<div id="form">
+    <table>
+      <tr>
+        <td>Title</td>
+        <td>Title Input</td>
+      </tr>
+      <tr>
+        <td>Author</td>
+        <td>Author Input</td>
+      </tr>
+      <tr>
+        <td>Pages</td>
+        <td>Pages Input</td>
+      </tr>
+      <tr>
+        <td>Read?</td>
+        <td>Read Toggle</td>
+      </tr>
+    </table>
+  </div>`;
+  const form = document.querySelector("#form");
+  if (showNewBook) {
+    render();
+  } else {
+    newBookBtn.innerHTML = formContent;
+  }
+  showNewBook = !showNewBook;
 }
 
 addBookToLibrary("John Henry", "Hiroshima", 150, false);
